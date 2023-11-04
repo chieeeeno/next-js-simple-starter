b50ffd306eb8cefec86d5b6a8bf94161e0715334
Tomoki Kikuchi <www.tomoki.jp@gmail.com>
2023-11-04 09:08:00 +0900

    Add new files and configurations for Next.js project

---
+++ .prettierrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // NOTE: デフォルト設定から変更
+  singleQuote: true,
+  jsxSingleQuote: true,
+  singleAttributePerLine: true,
+
+  // NOTE: デフォルト設定のまま
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+};
